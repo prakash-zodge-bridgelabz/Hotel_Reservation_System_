632b6d5de701207f68fdc40c64f4119dbe124dc6
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2024-01-02 17:42:14 +0530

    Use case 2 : Ability to find the cheapest hotel for a given date range

--- src/main/java/com/bridgelabz/Hotel_Reservation.java
+++ src/main/java/com/bridgelabz/Hotel_Reservation.java
@@ -14,14 +14,40 @@ package com.bridgelabz;
 
 // Use case 1 :
 // Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer…
+
+// Use case 2 : (Add weekday only)
+// Ability to find the cheapest Hotel for a given Date Range
+// - I/P – 10Sep2020, 11Sep2020
+// - O/P – Lakewood, Total Rates: $220
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+
 interface Hotel_Requirements{
     String getHotelName();
     int getRegularRatesForCustomer();
     int getTotalHotelsCreated();
+    String findCheapestHotel(String fromDate,String toDate) throws ParseException;
+    void setAllHotels(ArrayList<Hotel> allHotels);
 }
 class Hotel implements  Hotel_Requirements{
     String hotelName;
     int regularRatesForCustomer;
+    static ArrayList<Hotel> allHotels= new ArrayList<>();       //For storing all hotels
+    public void setAllHotels(ArrayList<Hotel> allHotels){
+        this.allHotels = allHotels;
+    }
+    Hotel(){
+        //For calling setAllHotels method
+    }
     static int totalHotelsCreated;      //Static count will be same for every hotels been created
     Hotel(String hotelName,int regularRatesForCustomer){
         this.hotelName = hotelName;
@@ -31,7 +57,48 @@ class Hotel implements  Hotel_Requirements{
     public String getHotelName()            {   return hotelName;               }
     public int getRegularRatesForCustomer() {   return regularRatesForCustomer; }
     public int getTotalHotelsCreated()      {   return totalHotelsCreated;      }
-    
+    static long totalDays;
+    public String findCheapestHotel(String fromDate,String toDate) throws ParseException {
+        totalDays = countDays(fromDate,toDate);
+        // Use case 2 : Weekday's only -> Assuming firstDay and lastDay are weekday's
+        //Iterate over all hotels and calculate price accordingly
+        if (allHotels.size() > 0) {
+            int best = 0;
+            int i  = 1;
+            while (i < allHotels.size()) {
+
+                double cost = allHotels.get(i).getPrice();
+                if (cost < allHotels.get(best).getPrice()) {        // Finding the cheapest hotel
+                    best = i;       // Storing index of arraylist which is cheapest
+                }
+                i++;
+            }
+            return allHotels.get(best).hotelName+", Total Rates: $"+allHotels.get(best).getPrice();
+        } else {
+            return "Hotel list is empty";
+        }
+    }
+    int getPrice() throws ParseException {
+        // Counting total days
+        int price=0;
+        if(totalDays <= 0){
+            // do nothing
+        }
+        else{
+            price += regularRatesForCustomer * totalDays;
+        }
+        return price;
+    }
+    long countDays(String firstDay,String lastDay){
+        String strDate1 = firstDay.replace("sep","09");
+        String strDate2 = lastDay.replace("sep","09");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMyyyy", Locale.ENGLISH);
+        LocalDateTime local_date1 = LocalDate.parse(strDate1, dtf).atStartOfDay();
+        LocalDateTime local_date2 = LocalDate.parse(strDate2, dtf).atStartOfDay();
+        long daysBetween = Duration.between(local_date1, local_date2).toDays();
+        // .betweeen method returns long
+        return daysBetween+1;
+    }
     @Override
     public String toString() {
         return "Hotel Name : "+hotelName+
--- src/test/java/com/bridgelabz/Hotel_Reservation_Test.java
+++ src/test/java/com/bridgelabz/Hotel_Reservation_Test.java
@@ -1,15 +1,43 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-// Test Case on Use Case 1 : Check count of hotels been created
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class Hotel_Reservation_Test {
+    ArrayList<Hotel> allHotels;       //For storing all hotels
+    Hotel Lakewood,Bridgewood,Ridgewood;
+    @Before
+    public void init(){
+        allHotels= new ArrayList<>();       //For storing all hotels
+        Lakewood = new Hotel("Lakewood",110);
+        Bridgewood = new Hotel("Bridgewood",160);
+        Ridgewood = new Hotel("Ridgewood",220);
+        allHotels.add(Lakewood);
+        allHotels.add(Bridgewood);
+        allHotels.add(Ridgewood);
+    }
+    // Use case 1, test case: Check count of hotels been created
     @Test
     public void addHotel(){
-        Hotel Lakewood = new Hotel("Lakewood",110);
-        Hotel Bridgewood = new Hotel("Bridgewood",110);
+        Lakewood = new Hotel("Lakewood",110);
+        Bridgewood = new Hotel("Bridgewood",160);
+        Ridgewood = new Hotel("Ridgewood",220);
         int actual = Bridgewood.getTotalHotelsCreated();
         Assert.assertEquals(2,actual);
+    }
+    // Use case 2 (Add weekday only), Test Case :
+    // Ability to find the cheapest Hotel for a given Date Range
+    // - I/P – 10Sep2020, 11Sep2020
+    // - O/P – Lakewood, Total Rates: $220
+    @Test
+    public void test_findCheapestHotel() throws ParseException {
+        Hotel h = new Hotel();
+        h.setAllHotels(allHotels);
+        String actual = h.findCheapestHotel("10sep2020","11sep2020");
+        Assert.assertEquals("Lakewood, Total Rates: $220",actual);
     }
 }
